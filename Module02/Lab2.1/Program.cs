e121ee0a2d410759b0c0143d01b053f78624f66f
geraldfingburke <geraldfingburke@gmail.com>
2025-02-04 20:23:38 -0500

    Add lab 2.1

---
+++ Module02/Lab2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * This is the Main() method of the Program.cs class.
+ * When the Main() method is called, the instructions we write here are performed.
+ * 
+ * Below is a list of commands we are using to print information to the console
+ */
+
+// Write a simple greeting
+Console.WriteLine("Howdy! I'm Gerald!");
+// Interesting fact 1
+Console.WriteLine("I can hold my breath for three years at a time.");
+// Interesting fact 2
+Console.WriteLine("I am made entirely of spaghetti.");
+// Interesting fact 3
+Console.WriteLine("My favorite color is Japanese Author Haruki Murakami.");
