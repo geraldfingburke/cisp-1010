799ba2c3b0ea1c2fd136804d4f91a1dc5ca90d1c
geraldfingburke <geraldfingburke@gmail.com>
2025-02-05 09:30:40 -0500

    Add lab 2.2

---
+++ Module02/Lab2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Lab 2.2 - ASCII Art
+ * The following is a piece of visual art using common characters.
+ * There is a certain amount of parity between what you see on the screen and what is printed to the console.
+ * You may notice lines that include a '\' have two of them.
+ * This is because '\' is a reserved character in our language.
+ * Specifically it is an escape character, which changes how a character in a string is interpreted.
+ * In this case, a single \ would be interpreted as escaping the next character.
+ * With the \\ we are telling the string that \ is to be interpreted literally.
+ */
+
+Console.WriteLine("~m~~~~~~m~~~~~~~~__~~~~~");
+Console.WriteLine("~~~~m~~~~~~~~`~~/  \\~~`~");
+Console.WriteLine("~~~~____~~~~m~~~\\  /~~~~");
+Console.WriteLine("~~~/    \\~~~~`~~~--~~~`~");
+Console.WriteLine("~~/  __  \\~~~~~~~`~~~~~~");
+Console.WriteLine("~~| |##| |~~~{}~~~{}~~~~");
+Console.WriteLine("~~| |##| |~~~/\\~~~/\\~~~~");
+Console.WriteLine("------------------------");
+Console.WriteLine("               (@##)    ");
+Console.WriteLine("--  --  --  --  --  --  ");
+Console.WriteLine("    (##@)             (#");
+Console.WriteLine("------------------------");
